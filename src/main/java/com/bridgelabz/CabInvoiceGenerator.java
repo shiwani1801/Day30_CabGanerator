078bc53faa405b4b1ededbb20cf454a72da69cb2
shiwani <shiwanipaunikar1@gmail.com>
2023-02-21 01:30:32 -0800

    Added:Given multiple ride shoud return total fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -12,4 +12,12 @@ public class CabInvoiceGenerator {
         else
             return totalFare;
     }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
+    }
 }
--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -10,4 +10,12 @@ public class CabInvoiceGeneratorTest {
         double test = obj.calculateFare(10, 15);
         Assert.assertEquals(115.0, test, 0.0);
     }
+    @Test
+    public void MonthFareTest() {
+        CabInvoiceGenerator obj = new CabInvoiceGenerator();
+        Ride[] rides = { new Ride(1, 23), new Ride(1, 2) };
+        double monthfare = obj.calculateFare(rides);
+        Assert.assertEquals(45.0, monthfare, 0.0);
+
+    }
 }
